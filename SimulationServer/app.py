e5f0042400e207ced301aef223b338b5459ec9ec
DNIIBOY <daniel.nettelfield@gmail.com>
2024-10-21 22:45:16 +0200

    Try a simulation thread

--- SimulationServer/app.py
+++ SimulationServer/app.py
@@ -4,11 +4,26 @@ from flask_sock import Sock
 from simple_websocket.ws import Server
 from time import sleep
 import gevent
+from threading import Thread
 
 valkey = Valkey(host="localhost", port=6379, db=0)
 sock = Sock()
 app = Flask(__name__)
 sock.init_app(app)
+
+
+def simulate_val_update():
+    """Simulate updating 'val' in valkey every 0.2 seconds."""
+    while True:
+        val = valkey.get("val")
+        val = int.from_bytes(val, byteorder="big")
+        val += 1
+        valkey.set("val", val.to_bytes(length=2, signed=False, byteorder="big"))
+        sleep(0.05)  # Simulate real-time data update
+
+
+# Start the background thread to simulate the value update
+Thread(target=simulate_val_update, daemon=True).start()
 
 
 @app.route("/")
@@ -32,4 +47,4 @@ def socket_test(ws: Server):
     while True:
         val = valkey.get("val")
         ws.send(val[::-1])  # Transmission reverses the byte order
-        gevent.sleep(0.2)
+        gevent.sleep(0.1)
