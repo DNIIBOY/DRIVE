d5c725e9a52922ccb521511beeb8011f2cd0c475
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2024-10-21 15:56:28 +0200

    Test websockets

--- SimulationServer/app.py
+++ SimulationServer/app.py
@@ -1,19 +1,29 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from valkey import Valkey
+from flask_sock import Sock
+from simple_websocket.ws import Server
+from time import sleep
 
 valkey = Valkey(host="localhost", port=6379, db=0)
+sock = Sock()
 app = Flask(__name__)
+sock.init_app(app)
 
 
 @app.route("/")
-def hello_valkey():
-    name = valkey.get("name")
-    if name:
-        name = name.decode()
-    return f"<p>Hello, {name}!</p>"
+def index():
+    return send_from_directory("static", "index.html")
 
 
 @app.route("/set/<name>")
-def set_name(name):
+def set_name(name: str):
     valkey.set("name", name)
     return f"<p>Set name to {name}</p>"
+
+
+@sock.route("/ws")
+def socket_test(ws: Server):
+    while True:
+        name = valkey.get("name")
+        ws.send(name)
+        sleep(0.2)
