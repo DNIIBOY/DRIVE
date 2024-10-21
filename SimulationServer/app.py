d8ff86c0d52ba9ca4a406eb1e793f6be44a23d01
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2024-10-21 15:08:02 +0200

    Simple valkey example

--- SimulationServer/app.py
+++ SimulationServer/app.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from valkey import Valkey
 
+valkey = Valkey(host="localhost", port=6379, db=0)
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hello_valkey():
+    name = valkey.get("name")
+    if name:
+        name = name.decode()
+    return f"<p>Hello, {name}!</p>"
+
+
+@app.route("/set/<name>")
+def set_name(name):
+    valkey.set("name", name)
+    return f"<p>Set name to {name}</p>"
