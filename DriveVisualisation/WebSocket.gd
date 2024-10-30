dca4b2a54cd5d7ab1722f9f42ed77e6834babf04
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2024-10-30 12:08:42 +0100

    Use signals for websocket

--- DriveVisualisation/WebSocket.gd
+++ DriveVisualisation/WebSocket.gd
@@ -1,25 +1,80 @@
+class_name WebSocketClient
 extends Node
 
-var socket = WebSocketPeer.new()
+@export var handshake_headers: PackedStringArray
+@export var supported_protocols: PackedStringArray
+var tls_options: TLSOptions = null
 
-func _ready():
-	socket.connect_to_url("localhost:8000/ws")
-	
-func _process(_delta):
-	
-	socket.poll()
-	if socket.get_ready_state() != WebSocketPeer.State.STATE_OPEN:
-		return
-	
-	
-	
-	#socket.get_packet()
-	var cars = []
-	while socket.get_available_packet_count():
-		#print("Got data from server: ", socket.get_packet().to_int32_array())
-		cars = socket.get_packet().to_int32_array()
-	
-	#print(cars)
-	for car in cars:
-		print(car | 4)
-	
+var socket := WebSocketPeer.new()
+var last_state := WebSocketPeer.STATE_CLOSED
+
+signal connected_to_server()
+signal connection_closed()
+signal message_received(message: Variant)
+
+func _on_message_received(message: Variant) -> void:
+    print("Received message: ", message)
+
+func connect_to_url(url: String) -> int:
+    socket.supported_protocols = supported_protocols
+    socket.handshake_headers = handshake_headers
+
+    var err := socket.connect_to_url(url, tls_options)
+    if err != OK:
+        return err
+
+    last_state = socket.get_ready_state()
+    return OK
+
+
+func send(message: String) -> int:
+    if typeof(message) == TYPE_STRING:
+        return socket.send_text(message)
+    return socket.send(var_to_bytes(message))
+
+
+func get_message() -> Variant:
+    if socket.get_available_packet_count() < 1:
+        return null
+    var pkt := socket.get_packet()
+    if socket.was_string_packet():
+        return pkt.get_string_from_utf8()
+    return pkt.to_int32_array()
+
+
+func close(code: int = 1000, reason: String = "") -> void:
+    socket.close(code, reason)
+    last_state = socket.get_ready_state()
+
+
+func clear() -> void:
+    socket = WebSocketPeer.new()
+    last_state = socket.get_ready_state()
+
+
+func get_socket() -> WebSocketPeer:
+    return socket
+
+
+func poll() -> void:
+    if socket.get_ready_state() != socket.STATE_CLOSED:
+        socket.poll()
+
+    var state := socket.get_ready_state()
+
+    if last_state != state:
+        last_state = state
+        if state == socket.STATE_OPEN:
+            connected_to_server.emit()
+        elif state == socket.STATE_CLOSED:
+            connection_closed.emit()
+    while socket.get_ready_state() == socket.STATE_OPEN and socket.get_available_packet_count():
+        message_received.emit(get_message())
+
+
+func _ready() -> void:
+    connect("message_received", Callable(self, "_on_message_received"))
+    connect_to_url("ws://localhost:8000/test_ws")
+
+func _process(_delta: float) -> void:
+    poll()
