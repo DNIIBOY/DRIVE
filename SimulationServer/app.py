2ef08c3766cb5a0dc6f5d0f6409534d715839431
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2024-11-04 15:23:34 +0100

    Add hw output

--- SimulationServer/app.py
+++ SimulationServer/app.py
@@ -46,4 +46,10 @@ def hardware_socket(ws: Server, hw_id: int):
             valkey.set(f"hw{hw_id}_brake", brake_pressure)
             continue
 
+        car_id = valkey.get(f"hw{hw_id}_car")
+        car_speed = valkey.get(f"hw{hw_id}_speed")
+        car_id = int(car_id.decode()) if car_id else 0
+        car_speed = int(car_speed.decode()) if car_speed else 0
+        val = (car_id << 22) | car_speed
+        ws.send(val.to_bytes(4, byteorder="big"))
         gevent.sleep(0.07)
