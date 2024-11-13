348d1fb1a1b09e17fd899130b3ab5f855631de92
CholstTheDev <CholstTheDev@gmail.com>
2024-11-13 21:30:09 +0100

    Added support for straight road

--- DriveVisualisation/Nodes/car_interpolate.gd
+++ DriveVisualisation/Nodes/car_interpolate.gd
@@ -8,26 +8,26 @@ var interpolation_coefficient = 2.3
 var active = false
 
 func _ready() -> void:
-	$Sprite2D.visible = false
+    $Sprite2D.visible = false
 
 func set_inactive():
-	active = false
-	$Sprite2D.visible = false
-	
+    active = false
+    $Sprite2D.visible = false
+    
 
 func set_new_target(target: float) -> void:
-	if active == false:
-		active = true
-		current_progress_ratio = target
-		$Sprite2D.visible = true
-	target_progress_ratio = clamp(target, 0, 1)
-	interpolate = 0
-		
+    if active == false:
+        active = true
+        current_progress_ratio = target
+        $Sprite2D.visible = true
+    target_progress_ratio = clamp(target, 0, 1)
+    interpolate = 0
+        
 func _process(delta: float) -> void:
-	if active:
-		interpolate += delta * interpolation_coefficient
-		current_progress_ratio = current_progress_ratio + (target_progress_ratio - current_progress_ratio) * interpolate
-		progress_ratio = current_progress_ratio
+    if active:
+        interpolate += delta * interpolation_coefficient
+        current_progress_ratio = current_progress_ratio + (target_progress_ratio - current_progress_ratio) * interpolate
+        progress_ratio = current_progress_ratio
 
-			
-			
+            
+            
