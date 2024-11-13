b129ff4a385306e7ea0f6b97c43bbaa150e847d5
Cholst <cholstthedev@gmail.com>
2024-11-13 15:08:31 +0100

    Added road settings

--- DriveVisualisation/generated_path.gd
+++ DriveVisualisation/generated_path.gd
@@ -7,15 +7,31 @@ var road_turns = 3
 var offset = Vector2(0,0)
 var inoutsize = 50
 
+signal baked_road
 
+@onready var input_road_turn_radius = $"../Camera2D/CanvasLayer/Control/RoadSettings/MarginContainer/VBoxContainer/HBoxContainer/RoadSettingContainer2/RadiusSettingElement2/SpinBox"
+@onready var input_road_width = $"../Camera2D/CanvasLayer/Control/RoadSettings/MarginContainer/VBoxContainer/HBoxContainer/RoadSettingContainer/WidthRoadSettingElement/SpinBox"
+@onready var input_road_turns = $"../Camera2D/CanvasLayer/Control/RoadSettings/MarginContainer/VBoxContainer/TurnsRoadSettingElement/SpinBox"
+@onready var input_bake_button = $"../Camera2D/CanvasLayer/Control/RoadSettings/MarginContainer/VBoxContainer/Button"
+
+func _update_values():
+    road_width = input_road_width.value
+    road_turn_radius = input_road_turn_radius.value
+    road_turn_diameter = road_turn_radius *2
+    road_turns = input_road_turns.value
+    
+    curve.clear_points()
+    bake_road()
+    baked_road.emit()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     
-    """transform.origin = Vector2(
-        (road_width + road_turn_diameter) * 0.5,
-        road_turn_diameter * road_turn_diameter * 0.5
-    )"""
+    input_bake_button.pressed.connect(_update_values)
+    bake_road()
+    
+
+func bake_road():
     
     #The first point, at top left corner
     curve.add_point(Vector2(0,0),
@@ -25,9 +41,7 @@ func _ready() -> void:
     curve.add_point(
         Vector2(road_width+road_turn_radius,0),
         Vector2(inoutsize,0),
-        Vector2(-inoutsize,0))
-    
-        
+        Vector2(-inoutsize,0))    
     
     for i in range(road_turns):
         #The point at end of the turn to the right-side turn
@@ -55,3 +69,4 @@ func _ready() -> void:
     curve.add_point(Vector2(road_width + road_turn_diameter, road_turn_diameter * (road_turns*2)))
 
     print(curve.get_baked_length())
+    
