fbbfd769d1cc6effdbf3f08983bf197f815ecb94
Cholst <cholstthedev@gmail.com>
2024-11-18 14:35:22 +0100

    Made the front car actually use logic

--- SimulationServer/idm.py
+++ SimulationServer/idm.py
@@ -5,7 +5,7 @@ from config import SimulationConfig
 
 
 def idm(car: Car, config: SimulationConfig):
-    acceleration = 0
+    #acceleration = 0
 
     # if car.next:
     #     if time() > car.time_to_next_reaction:
@@ -24,21 +24,23 @@ def idm(car: Car, config: SimulationConfig):
     #             car.time_to_next_reaction += 0.2
     #     else:
     #         acceleration = car.accel
-
+    v = car.speed  # Aktuelle hastighed
+    v0 = 277  # car.reference_speed  # Ønskede hastighed
+    a_max = config.car_max_accel  # Max acceleration
+    
     if car.next:
-        v = car.speed  # Aktuelle hastighed
-        v0 = 277  # car.reference_speed  # Ønskede hastighed
         delta_v = car.next.speed - car.speed
         s0 = config.target_distance  # Ønskede minimum afstand
-        s = max(car.next.position - car.position, 0.01)  # Aktuelle afstand
+        s = max(car.next.position - car.position, 0.01) + config.car_length  # Aktuelle afstand
         T = 0.5  # "Time Headway", den ønskede afstand til forankørende bil i sekunder
-        a_max = config.car_max_accel  # Max acceleration
-        b = 80  # Komfortabel bremseværdi
+        b = 20  # Komfortabel bremseværdi
 
-        accel_formular_term_1 = 1 - (v / v0)**4
+        accel_formular_term_1 = (v / v0)**4
         s_star = s0 + T * v + (v * delta_v) / (2 * math.sqrt(a_max * b))
         accel_formular_term_2 = s_star / s
-        acceleration = a_max * (accel_formular_term_1 - accel_formular_term_2**2)
+        acceleration = a_max * (1 - accel_formular_term_1 - accel_formular_term_2**2)
+    else:
+        acceleration = a_max * (1 - (v / v0)**2)
 
     return acceleration * config.update_interval
 
