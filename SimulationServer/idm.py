f38241a7e61d92c45bb8930a7604d16c0fd173a4
Cholst <cholstthedev@gmail.com>
2024-11-18 12:05:29 +0100

    Hopefully fixed IDM

--- SimulationServer/idm.py
+++ SimulationServer/idm.py
@@ -33,16 +33,17 @@ def idm(car: Car, config: SimulationConfig):
         delta_v = car.next.speed - car.speed
         s0 = config.target_distance  # Ønskede minimum afstand
         s = max(car.next.position - car.position, 0.01) # Aktuelle afstand
-        T = 2 # "Time Headway", den ønskede afstand til forankørende bil i sekunder
+        T = 0.5 # "Time Headway", den ønskede afstand til forankørende bil i sekunder
         a_max = config.car_max_accel # Max acceleration
-        b = 20 # Komfortabel bremseværdi
+        b = 80 # Komfortabel bremseværdi
 
         accel_formular_term_1 = 1 - (v / v0)**4
-        accel_formular_term_2 = (s0 + T * v - s + (v * delta_v) / (2 * math.sqrt(a_max * b)))
-        acceleration = a_max * accel_formular_term_1 * accel_formular_term_2
+        s_star = s0 + T * v + (v * delta_v) / (2 * math.sqrt(a_max * b))
+        accel_formular_term_2 = s_star / s
+        acceleration = a_max * (accel_formular_term_1 - accel_formular_term_2**2)
 
-        if car.id == 1:
-            print(f"Rel dist: {car.next.position - car.position}, speed: {car.speed}, accel: {acceleration}")
+        #if car.id == 1:
+        #    print(f"Rel dist: {car.next.position - car.position}, speed: {car.speed}, accel: {acceleration}")
     return acceleration * config.update_interval
 
     # Vores system skal KLARE de stokatiske udfordringer
