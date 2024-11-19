dd1945ec91094308d3a653dc0b1f1d0f8416f124
Mathias Sørensen <mkrs24@student.aau.dk>
2024-11-19 17:15:28 +0100

    made a base configuration for the simulation

--- SimulationServer/config.py
+++ SimulationServer/config.py
@@ -4,21 +4,20 @@ from valkey import Valkey
 
 @dataclass
 class SimulationConfig:
-    initial_speed: int = 200
+    initial_speed: int = 277
     speed_limit: int = 277
     kill_distance: int = 65535
-
-    spawn_distance: int = 500
-    target_distance: int = 10 + 15  # 15 er pga car_length ikke er implementeret
-
-    update_interval: float = 0.05
     car_length: int = 15
-    car_max_accel: float = 80
 
-    time_headway: float = 0.5
+    spawn_distance: int = 700
+    target_distance: int = 25
+    update_interval: float = 0.05
+    car_max_accel: float = 30
+
+    time_headway: float = 1.5
     comfortable_breaking_value: float = 20
 
-    speed_limit_deviation: int = 10
+    speed_limit_deviation: int = 0
     percieved_distance_spread: float = 1
 
     def read(self, valkey: Valkey) -> None:
