41eb65280f1fae3a1a6263d76f3c5624a10e835d
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2024-11-19 12:32:52 +0100

    Wow

--- SimulationServer/idm.py
+++ SimulationServer/idm.py
@@ -7,7 +7,8 @@ from config import SimulationConfig
 
 def idm(car: Car, config: SimulationConfig):
     v = car.speed  # Aktuelle hastighed
-    v0 = np.random.normal(config.speed_limit, config.speed_limit_deviation)   # car.reference_speed  # Ønskede hastighed
+    # car.reference_speed  # Ønskede hastighed
+    v0 = np.random.normal(config.speed_limit, config.speed_limit_deviation)
     a_max = config.car_max_accel  # Max acceleration
 
     if car.next:
@@ -18,13 +19,14 @@ def idm(car: Car, config: SimulationConfig):
         T = config.time_headway  # "Time Headway", den ønskede afstand til forankørende bil i sekunder
         b = config.comfortable_breaking_value  # Komfortabel bremseværdi
 
-        distance_perception_deviation = np.random.normal(0, config.percieved_distance_spread)  # En normalfordeling til percieved distance
+        distance_perception_deviation = np.random.normal(
+            0, config.percieved_distance_spread)  # En normalfordeling til percieved distance
         s_percieved = max(s + distance_perception_deviation, 0.01)
 
         accel_formular_term_1 = (v / v0) ** 4
         s_star = (s0 + T * v
-            + max((v * delta_v) / (2 * math.sqrt(max(a_max, 1) * max(b, 1))), 0)
-        )
+                  + max((v * delta_v) / (2 * math.sqrt(max(a_max, 1) * max(b, 1))), 0)
+                  )
 
         accel_formular_term_2 = (
             s_star / s_percieved
