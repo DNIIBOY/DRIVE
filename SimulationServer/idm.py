f8cad92430ba3301dda58eab08620842c17a2a65
smmarkvard <smmarkvard@gmail.com>
2024-11-28 10:40:13 +0100

    Update idm.py

--- SimulationServer/idm.py
+++ SimulationServer/idm.py
@@ -19,21 +19,12 @@ def idm(car: Car, config: SimulationConfig):
         # "Time Headway", den ønskede afstand til forankørende bil i sekunder
         T = config.time_headway
 
-        # Stokastisk relativ hastighed
-
-        # Normalfordelt afstandsbedømmelse
-        estimated_gap = max(np.random.normal(s, config.percieved_distance_spread), 0.01)  # Normal value 0.1
-        delta_v_percieved = np.random.normal(delta_v, config.percieved_speed_spread)  # Normal value 0.1
-        # if s > config.percieved_cut_off:
-        # else:
-        #     estimated_gap = s
-        #     delta_v_percieved = delta_v
         accel_formular_term_1 = (v / v0) ** 4
 
-        dynamic_term = (v * delta_v_percieved) / (2 * config.braking_factor)
+        dynamic_term = (v * delta_v) / (2 * config.braking_factor)
         s_star = s0 + T * v + max(0, dynamic_term)
 # Reducer exponent for at få mindre aggressiv bremse
-        accel_formular_term_2 = (s_star / estimated_gap) ** 2
+        accel_formular_term_2 = (s_star / s) ** 2
         acceleration = a_max * (1 - accel_formular_term_1 - accel_formular_term_2)
     else:
         acceleration = a_max * (1 - (v / v0) ** 2)
