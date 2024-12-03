baa15c5a7a8fe0d7ec59560412ef5bde39157625
Rasmus <rasser1357@gmail.com>
2024-12-03 13:22:45 +0100

    led i2c controller

--- HardwareController/components/websocket_handler/websocket_handler.c
+++ HardwareController/components/websocket_handler/websocket_handler.c
@@ -2,16 +2,19 @@
 #include "esp_log.h"
 
 #include "HD44780.h"
+#include "pcf8574.h"
 
 #define WIFI_MAXIMUM_RETRY 10
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT BIT1
 
 #define LCD_ADDR 0x27
+#define LED_ADDR 0x20
+
 #define SDA_LCD_PIN 7
 #define SCL_LCD_PIN 6
-#define SDA_LED_PIN 
-#define SCL_LED_PIN 
+#define SDA_LED_PIN 15
+#define SCL_LED_PIN 14
 
 #define LCD_COLS 16
 #define LCD_ROWS 2
@@ -21,6 +24,11 @@ uint16_t recommended_speed;
 
 char lcdbuffer_line_1[6];
 char lcdbuffer_line_2[6];
+
+static void set_leds(uint8_t led_data) {
+    // Write LED data to the I/O expander
+    pcf8574_write(LED_ADDR, led_data);
+}
 
 //static EventGroupHandle_t s_wifi_event_group;
 
@@ -37,6 +45,8 @@ static void websocket_event_handler(void *handler_args, esp_event_base_t base, i
 
             lcd_set_cursor(0, 1);
             lcd_write_str("Advised:     kmt");
+
+            pcf8574_init(LED_ADDR, SDA_LED_PIN, SCL_LED_PIN);
             break;
 
         case WEBSOCKET_EVENT_DISCONNECTED:
@@ -54,7 +64,7 @@ static void websocket_event_handler(void *handler_args, esp_event_base_t base, i
             //ESP_LOGI("WebSocket_handler", "Data received: length=%d, data=0x%08X", event->data_len, value);
             current_speed = (value & 0xFFF) * 0.36;
             recommended_speed = ((value >> 12) & 0xFFF) * 0.36;
-            LED_data = (value >> 24) & 0xFF;
+            uint8_t LED_data = (value >> 24) & 0xFF;
 
             sprintf(lcdbuffer_line_1, "%03d", current_speed);  // Ensure 3 digits
             sprintf(lcdbuffer_line_2, "%03d", recommended_speed);  // Ensure 3 digits
@@ -65,7 +75,7 @@ static void websocket_event_handler(void *handler_args, esp_event_base_t base, i
             lcd_set_cursor(9, 1);
             lcd_write_str(lcdbuffer_line_2);
 
-            
+            set_leds(LED_data);
 
             break;
         default:
